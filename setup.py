41781514bef35d97606a1c066f9ba874f28843a3
Ethosa <social.ethosa@gmail.com>
2022-08-14 13:31:54 +0700

    update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Ethosa",
     author_email="social.ethosa@gmail.com",
-    version="0.2.1",
+    version="0.2.2",
     url="https://github.com/ethosa/avoscript",
     install_requires=[
         "colorama",
