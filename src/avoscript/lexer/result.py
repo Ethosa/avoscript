7526e1bb4191ed3c21fd67dde8d2250c19bf682a
Ethosa <social.ethosa@gmail.com>
2022-08-29 14:24:10 +0700

    add `test-package` package

--- src/avoscript/lexer/result.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class Result:
-    def __init__(self, value, pos):
-        self.value = value
-        self.pos = pos
-
-    def __repr__(self) -> str:
-        return f"Result({self.value}, {self.pos})"
